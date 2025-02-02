a75eedb363a3fbce271e972985c4fb183ed6034d
technicalclipper <technicalclipper@gmail.com>
2025-02-02 23:00:42 +0530

    connected frontend to fundnewuser api to receive funds to embedded wallet from server wallet

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,26 @@
 "use client";
 import "./components.css";
 import { usePrivy, useLogin } from "@privy-io/react-auth";
+import axios from "axios";
 
 export default function Header() {
     const { login, logout, ready, authenticated } = usePrivy(); // Get authentication state
-
+    
+    const fundNewUser = async (address) => {
+        try {
+          const response = await axios.post("/api/fundnewuser", { address: address });
+          console.log("Response:", response.data);
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+    
     const { login: loginUser } = useLogin({
         onComplete: ({ user, isNewUser, wasAlreadyAuthenticated, loginMethod, linkedAccount }) => {
-            
+            if(isNewUser){
+                //console.log(user.wallet.address);         debugging purposes
+                fundNewUser(user.wallet.address);
+              }
            
         },
         onError: (error) => {
